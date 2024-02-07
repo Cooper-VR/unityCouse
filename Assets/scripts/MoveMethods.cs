d51baac47382b3d7c96f95d1158ba75b8beac2a2
Cooper Bower <cooperjbower@gmail.com>
2024-02-07 14:32:06 -0700

    setting up player controls

--- Assets/scripts/MoveMethods.cs
+++ Assets/scripts/MoveMethods.cs
@@ -5,10 +5,41 @@ using UnityEngine;
 public class MoveMethods : MonoBehaviour
 {
     public obectType type;
+    private Rigidbody2D rb;
+    private float speed = 900f;
 
     public enum obectType
     {
         Player,
         Object
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move()
+    {
+
+    }
+    public void Move(Vector2 MoveVector)
+    {
+        if (rb.velocity.magnitude < 25)
+        rb.AddForce(MoveVector * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        Vector2 moveVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (type == obectType.Player)
+        {
+            Move(moveVector);
+        }
+        else
+        {
+            Move();
+        }
     }
 }
