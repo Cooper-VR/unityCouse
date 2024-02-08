657c09f5ff7119e52b3a9ab250a7791f4fb88f4f
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2024-02-08 13:35:33 -0700

    spawning added

--- Assets/scripts/RockSpawner.cs
+++ Assets/scripts/RockSpawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class RockSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float spawnInterval;
+    public GameObject RockPrefab;
+
+    private void SpawnRock()
     {
-        
+        Vector3 spawnPos = new Vector3((Random.value - 0.5f) * 500, (Random.value - 0.5f) * 500, 0).normalized * 600f + transform.position;
+
+        Instantiate(RockPrefab, spawnPos, RockPrefab.transform.rotation, transform);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        spawnInterval = Mathf.Pow(1.1f, Time.timeSinceLevelLoad);
+    }
+
+    private void Update()
+    {
+        spawnInterval -= Time.deltaTime;
+
+        if (spawnInterval <= 0)
+        {
+            SpawnRock();
+            spawnInterval = Mathf.Pow(1.1f, -0.5f * Time.timeSinceLevelLoad) + 1;
+        }
     }
 }
